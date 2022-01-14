8e5a4a036f20dc833090c169f5dd8e345a391f25
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-01-14 17:16:54 -0300

    feat(tree interaction): add methods to trees
    #pretty_print outputs to the console a tree-like diagram showing it's nodes values
    and connections and greatly improves readability of the tree.
    #insert adds a new node at any leaf position of the tree while keeping the tree's
    search properties, but not it's balance.
    #delete removes or redirects the pointer to the deleted node, depending on it's
    position in the tree.
    #min_sucessor is a recursion support used by #delete method to find which node
    should take the deleted node's position in the tree in case it had childrens.
    #find checks if a node value exists within the tree and returns that node.
    #level_order prints all nodes values in tree level order (like breadth search)
    or as specified if a block is provided.
    #preorder should print node values like in a deepth preorder search, but needs
    correction.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,25 +1,17 @@
 # frozen_string_literal: true
 
-# Used to store the each node's data, as well as it's childrens
+# Used to store each node's data and left 'n right pointers
 class Node
-  include Comparable
-  attr_reader :data, :left, :right
+  attr_accessor :data, :left, :right
 
-  def initialize(data, left = nil, right = nil)
-    @data = data
+  def initialize(value, left = nil, right = nil)
+    @data = value
     @left = left
     @right = right
-    p "data: #{data}"
-    p "left: #{left}, right: #{right}"
-  end
-
-  def <=>(other)
-    data <=> other.data
   end
 end
 
-# Accepts an array when initialized. The Tree class have a root attribute which
-# uses the return value of #build_tree.
+# Stores the root of the tree, which comes from #build_tree
 class Tree
   attr_reader :root
 
@@ -29,13 +21,105 @@ class Tree
 
   def build_tree(array)
     return Node.new(array[0]) if array.size == 1
+    return nil if array.empty?
 
-    # return nil if array[0].nil?
-
-    left = Tree.new(array[0..array.size / 2 - 1])
-    right = Tree.new(array[array.size / 2 + 1..array.size - 1])
-    @root = Node.new(array[array.size / 2], left.root.data, right.root.data)
+    left_node = build_tree(array[0..array.size / 2 - 1])
+    right_node = build_tree(array[array.size / 2 + 1..array.size])
+    Node.new(array[array.size / 2], left_node, right_node)
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value, node = @root)
+    return if value == node.data
+    return node.left = Node.new(value) if value < node.data && node.left.nil?
+    return node.right = Node.new(value) if value > node.data && node.right.nil?
+
+    if value < node.data
+      insert(value, node.left)
+    else
+      insert(value, node.right)
+    end
+  end
+
+  def delete(value, node = @root, parent = nil)
+    return if value != node.data && node.right.nil? && node.left.nil?
+    return parent.left = nil if node.data == value && node.right.nil? && node.left.nil? && parent.left == node
+    return parent.right = nil if node.data == value && node.right.nil? && node.left.nil? && parent.right == node
+    return parent.right = node.right if value == node.data && node.right && node.left.nil?
+    return parent.left = node.left if value == node.data && node.left && node.right.nil?
+
+    if value == node.data && node.left && node.right
+      next_node_value = min_successor(node.right).data
+      delete(next_node_value)
+      node.data = next_node_value
+      return
+    end
+
+    if value < node.data
+      delete(value, node.left, node)
+    else
+      delete(value, node.right, node)
+    end
+  end
+
+  def min_successor(node)
+    return node if node.left.nil?
+
+    min_successor(node.left)
+  end
+
+  def find(value, node = @root)
+    return node if node.data == value
+    return if node.data > value && node.left.nil?
+    return if node.data < value && node.right.nil?
+
+    if node.data > value
+      find(value, node.left)
+    else
+      find(value, node.right)
+    end
+  end
+
+  def level_order(queue = [@root], level_order_list = [], &block)
+    if queue.empty?
+      if block_given?
+        level_order_list.each do |node|
+          yield node
+        end
+      else
+        p level_order_list.map(&:data)
+      end
+      return
+    end
+
+    queue.size.times do
+      level_order_list << queue[0]
+      queue << queue[0].left if queue[0].left != nil
+      queue << queue[0].right if queue[0].right != nil
+      queue.delete_at(0)
+    end
+    level_order(queue, level_order_list, &block)
+  end
+
+#   def preorder(node = @root, preorder_list = [])
+#     preorder_list << node
+#     preorder(node.left, preorder_list) if node.left
+#     preorder(node.right, preorder_list) if node.right
+#     if block_given? && node == root
+#       preorder_list.each do |node|
+#         yield node
+#       end
+#     else
+#       p preorder_list.map(&:data)
+#     end
+#   end
 end
 
-Tree.new([1, 3, 4, 6, 9, 20, 23])
+test = Tree.new([3, 5, 6, 8, 11, 15, 18])
+test.pretty_print
+test.preorder
